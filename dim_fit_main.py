7d126e1a094a3e6ac7f0aa994ee5bf712c4dc4d5
Alexander <47395086+aliemen@users.noreply.github.com>
2022-04-16 17:35:06 +0200

    n-th dimensional derivative
    I have added the ability to calculate the n-th derivative of a function represented by the pair of an *order*- and a *params* array. Since it is a polynomial, it does not need to be approximated, but can simply be calculated algebraically in any variable.

--- dim_fit_main.py
+++ dim_fit_main.py
@@ -27,13 +27,64 @@ import itertools
 
 def poly_n_val(order, params, *args, get_func=True, only_func=False):    
     if only_func:
-        return lambda *x: _eval_polynomial_order(order, params, *x)
+        return _get_callable(order, params) # _eval_polynomial_order(order, params, *x)
 
     ZZ = _eval_polynomial_order(order, params, *args)    
     if get_func:
-        return ZZ, lambda *x: _eval_polynomial_order(order, params, *x)
+        return ZZ, _get_callable(order, params) # _eval_polynomial_order(order, params, *x)
     else:
         return ZZ
+
+
+
+def n_th_derivative(order:list, params:list, index:int, *args, get_func=True, only_func=False, n_th=1):
+    '''
+    Parameters
+    ----------
+    order : array_like
+        DESCRIPTION.
+    params : array_like
+        DESCRIPTION.
+    index : int
+        DESCRIPTION.
+    *args : array_like objects (optional)
+        Contains the evaluation values for every axis.
+    get_func : bool, optional
+        Does return a callable function of the n-th derivative if true. The default is True.
+    only_func : bool, optional
+        Does return only the callable function if true (no otherwise evaluation). The default is False.
+    n_th : TYPE, optional
+        DESCRIPTION. The default is 1.
+    '''
+    
+    # first, compute the order and params for the derivative
+    new_order, new_params = np.array(order), np.array(params)
+    for n in range(n_th): # do n_th derivatives
+        # get orders and params for derivative, consistency check for index will be done in _first_derivative
+        new_order, new_params = _first_derivative(new_order, new_params, index)
+    
+    # manage returns...
+    if only_func:
+        return _get_callable(new_order, new_params)     
+    
+    # evaluate the n_th derivatives at that *args points, consistency checks will be done in _eval_polynomial_order
+    ZZ = _eval_polynomial_order(new_order, new_params, *args)
+    if get_func:
+        return ZZ, _get_callable(new_order, new_params)
+    else:
+        return ZZ
+        
+    
+    
+    
+
+
+def _get_callable(order:list, params:list):
+    # do consistency check
+    assert len(order) == len(params), "Number of parameters does not match the summands"
+    
+    return lambda *x: _eval_polynomial_order(order, params, *x)
+        
     
     
 
@@ -170,6 +221,54 @@ def _get_exponential_indizes(max_orders, max_total_order):
                 used_order.append(index_tupel)
         
     return np.array(used_order)
+
+
+
+def _first_derivative(order:list, params:list, index:int):
+    '''
+    Parameters
+    ----------
+    order : array_like
+        "exponentials_array" that is returned by e.g. "poly_n_fit(...)".
+    params : array_like
+        "x_params" that is returned by e.g. "poly_n_fit(...)".
+    index : int
+        Specifies the variable in which the first derivative is to be calculated.
+        "index=0" will result in the derivative with respect to the first variable.
+
+    Returns
+    -------
+    new_order : array_like
+        Gives the new "exponentials_array" corresponding to the first derivative.
+    new_params : array_like
+        Gives the new "x_params" corresponding to the first derivative.
+    '''
+    
+    # consistency check (does this variable exist?)
+    assert index < len(order[0]) and 0 <= index, "Index and function dimensions do not match. Please perform derivative with respect to a variable that exists."
+    
+    # need them as Numpy array (mainly because of the "+=" and scalar-array multiplication)
+    order  = np.array(order)
+    params = np.array(params)
+    
+    new_orders = []
+    new_params = []
+    
+    # tmp_param and tmp_order are potentially modified in this loop, keep that in mind
+    for tmp_param, tmp_order in zip(params.copy(), order.copy()):
+        if tmp_order[index] == 0: # d/dx (constant * x^0) = 0
+            new_params.append(0.0)
+            new_orders.append(tmp_order)
+        else:
+            new_params.append(tmp_param*tmp_order[index])
+            
+            tmp_order[index] = tmp_order[index] - 1
+            new_orders.append(tmp_order)
+            
+    return np.array(new_orders), np.array(new_params)
+
+
+
 
 
 
